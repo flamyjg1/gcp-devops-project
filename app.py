0c75aad50fb05d1bd4787a7ce5154f8072da25fb
flamyjg1 <129269485+flamyjg1@users.noreply.github.com>
2024-04-26 17:59:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<div style=\"text-align: center;\"><h1>Hello Everyone, I am Flamy!</h1><p>Welcome to GCP DevOps PROJECT 1.</p><p></p></div>'
+    return '<div style=\"text-align: center;\"><h1>Hello Everyone, I am Flamy!</h1><p>Welcome to GCP DevOps PROJECT in Dev environment.</p><p></p></div>'
